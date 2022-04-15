a18e381688d0823a8ecf393028440e07b4207d9e
Maharaja <2200092@ecc.ac.jp>
2022-04-15 16:15:31 +0900

    test

---
+++ app/src/main/java/com/example/team_a/data/CreateUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.team_a.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.team_a.R
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
