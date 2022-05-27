f1aab51f010404aa00d6a02d12829f13787cd009
takemura <koyo141287@gmail.com>
2022-05-27 13:50:26 +0900

    timeline3

---
+++ app/src/main/java/com/example/team_a/ui/login/Myapplication.kt
@@ -0,0 +1,11 @@
+package com.example.team_a.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.team_a.R
+
+class Myapplication : AppCompatActivity() {
+
+    var apiUrl = "https://click.ecc.ac.jp/ecc/whisper_a/public_html/mysqlConnect.php"
+    var loginUserId = ""
+}
